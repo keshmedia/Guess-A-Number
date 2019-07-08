9a5f5cd90d3b845c98eba4bf3ab616169349efcc
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-07-08 11:40:42 +0100

    Add files via upload

---
+++ Guess A Number/Guess A Number/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Guess_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ // Create a new instance of our number guesser class
+ var numberGuesser = new NumberGuesser();
+ // Change the default maximum number to 200
+ numberGuesser.MaximumNumber = 200;
+ // Ask the user to think of a number
+ numberGuesser.InformUser();
+ // Discover the number the user is thinking of
+ numberGuesser.DiscoverNumber();
+ // Announce the results
+ numberGuesser.AnnounceResults();
+        } } }
